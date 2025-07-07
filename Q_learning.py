025b51bf54972cb8bf3729985c0b681fa2d2ab66
pedross3 <pe.santos91@gmail.com>
2025-07-07 16:48:36 +0200

    Adjustments to training

--- Q_learning.py
+++ Q_learning.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 # -----------
 rewards = []
 episodes = []
+
 def train_q_learning(env,
                      no_episodes,
                      epsilon,
@@ -16,12 +17,15 @@ def train_q_learning(env,
                      epsilon_decay,
                      alpha,
                      gamma,
-                     q_table_save_path="q_table.npy"):
+                     q_table_save_path="q_table.npy",
+                     train = None):
 
     # Initialize the Q-table:
     # -----------------------
-    q_table = np.zeros((env.y_size, env.x_size, env.action_space.n))
-
+    if train == True:
+        q_table = np.zeros((env.y_size, env.x_size, env.action_space.n))
+    if train == False:
+        q_table = np.load("q_table.npy")
     # Q-learning algorithm:
     # ---------------------
     #! Step 1: Run the algorithm for fixed number of episodes
@@ -79,12 +83,16 @@ def train_q_learning(env,
     np.save(q_table_save_path, q_table)
     print("Saved the Q-table.")
     plt.plot(episodes, rewards)
+    plt.title("Reward vs Epoch")
+    plt.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("Reward")
     plt.show()
 
 # Function 2: Visualize the Q-table
 # -----------
 def visualize_q_table(hell_state_coordinates = [(2,6), (2, 10), (4, 1), (4, 9), (8, 4), (9, 7), (12, 7), (14, 5)],
-                      goal_coordinates=[1,6],
+                      goal_coordinates=tuple([1,6]),
                       wall_states = [],
                       actions=["Up", "Down", "Right", "Left"],
                       q_values_path="q_table.npy"):
@@ -108,7 +116,7 @@ def visualize_q_table(hell_state_coordinates = [(2,6), (2, 10), (4, 1), (4, 9), 
             # Mask the goal state's Q-value for visualization:
             # ------------------------------------------------
             mask = np.zeros_like(heatmap_data, dtype=bool)
-            mask[tuple(goal_coordinates)] = True
+            mask[goal_coordinates] = False
             mask[hell_state_coordinates[0]] = True
             mask[hell_state_coordinates[1]] = True
             mask[hell_state_coordinates[2]] = True
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from Q_learning import train_q_learning, visualize_q_table
 
 # User definitions:
 # -----------------
-train = True
+train = False
 visualize_results = True
 
 random_initialization = True  # If True, the Q-table will be initialized randomly
@@ -14,7 +14,7 @@ random_initialization = True  # If True, the Q-table will be initialized randoml
 learning_rate = 0.01  # Learning rate
 gamma = 0.99  # Discount factor
 epsilon = 1.0  # Exploration rate
-epsilon_min = 0.01  # Minimum exploration rate
+epsilon_min = 0.1  # Minimum exploration rate
 
 # 
 # 
@@ -41,21 +41,40 @@ hell_state_coordinates = [np.array([2, 6]), np.array([2, 10]), np.array([4, 1]),
 
 # Execute:
 # --------
-if train:
+if train == True:
     # Create an instance of the environment:
     # --------------------------------------
     env = create_env(random_initialization=random_initialization)
     print("main: iniciando processo")
     # Train a Q-learning agent:
     # -------------------------
+
     train_q_learning(env=env,
                      no_episodes=no_episodes,
                      epsilon=epsilon,
                      epsilon_min=epsilon_min,
                      epsilon_decay=epsilon_decay,
                      alpha=learning_rate,
-                     gamma=gamma)
+                     gamma=gamma,
+                     train = train)
     
+if train == False:
+    # Create an instance of the environment:
+    # --------------------------------------
+    env = create_env(random_initialization = False)
+    print("main: iniciando processo")
+    # Train a Q-learning agent:
+    # -------------------------
+
+    train_q_learning(env=env,
+                     no_episodes=no_episodes,
+                     epsilon = 0,
+                     epsilon_min = 0,
+                     epsilon_decay = 0,
+                     alpha=learning_rate,
+                     gamma=gamma,
+                     train = train)
+
 if visualize_results:
     # Visualize the Q-table:
     # ----------------------
